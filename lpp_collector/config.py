1fb13c731120960cb61df68f6e88c6907e46e33c
f0reachARR <f0reach@f0reach.me>
2026-01-03 17:58:53 +0900

    API URL fix

--- lpp_collector/config.py
+++ lpp_collector/config.py
@@ -6,7 +6,7 @@ import lpp_collector
 LPP_BASE_URL = (
     os.environ["LPP_BASE_URL"]
     if "LPP_BASE_URL" in os.environ
-    else "http://10.0.1.34:18480"
+    else "https://se.is.kit.ac.jp/lpp_api/"
 )
 
 LPP_CONSENT_TEXT = """
